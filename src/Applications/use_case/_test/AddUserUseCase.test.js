536c2a2c3198565ea360dddb82212612242278c2
Elizra <azrielwah@gmail.com>
2022-04-23 21:02:49 +0700

    init tugas 1

--- src/Applications/use_case/_test/AddUserUseCase.test.js
+++ src/Applications/use_case/_test/AddUserUseCase.test.js
@@ -3,8 +3,11 @@ const RegisteredUser = require('../../../Domains/users/entities/RegisteredUser')
 const UserRepository = require('../../../Domains/users/UserRepository');
 const PasswordHash = require('../../security/PasswordHash');
 const AddUserUseCase = require('../AddUserUseCase');
- 
+
 describe('AddUserUseCase', () => {
+  /**
+   * Menguji apakah use case mampu mengoskestrasikan langkah demi langkah dengan benar.
+   */
   it('should orchestrating the add user action correctly', async () => {
     // Arrange
     const useCasePayload = {
@@ -17,11 +20,11 @@ describe('AddUserUseCase', () => {
       username: useCasePayload.username,
       fullname: useCasePayload.fullname,
     });
- 
+
     /** creating dependency of use case */
     const mockUserRepository = new UserRepository();
     const mockPasswordHash = new PasswordHash();
- 
+
     /** mocking needed function */
     mockUserRepository.verifyAvailableUsername = jest.fn()
       .mockImplementation(() => Promise.resolve());
@@ -29,16 +32,16 @@ describe('AddUserUseCase', () => {
       .mockImplementation(() => Promise.resolve('encrypted_password'));
     mockUserRepository.addUser = jest.fn()
       .mockImplementation(() => Promise.resolve(expectedRegisteredUser));
- 
+
     /** creating use case instance */
     const getUserUseCase = new AddUserUseCase({
       userRepository: mockUserRepository,
       passwordHash: mockPasswordHash,
     });
- 
+
     // Action
     const registeredUser = await getUserUseCase.execute(useCasePayload);
- 
+
     // Assert
     expect(registeredUser).toStrictEqual(expectedRegisteredUser);
     expect(mockUserRepository.verifyAvailableUsername).toBeCalledWith(useCasePayload.username);
